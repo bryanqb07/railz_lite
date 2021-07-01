2296ac77346f0e916adc612c0de0e8b871d41b6a
bryan lynch <bml312@nyu.edu>
2021-07-01 14:33:51 +0800

    add module to namespace

--- lib/railz_lite.rb
+++ lib/railz_lite.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 require_relative "railz_lite/version"
+require_relative 'railz_lite/controllers/controller_base'
+require_relative 'railz_lite/models/sql_object'
 
 module RailzLite
   class Error < StandardError; end
--- lib/railz_lite/models/sql_object.rb
+++ lib/railz_lite/models/sql_object.rb
@@ -2,52 +2,53 @@ require_relative 'db_connection'
 require 'active_support/inflector'
 require 'byebug'
 
-class SQLObject
-  def self.columns
-    @columns ||= DBConnection.execute2(<<-SQL)
+module RailzLite
+  class SQLObject
+    def self.columns
+      @columns ||= DBConnection.execute2(<<-SQL)
       SELECT
         *
       FROM
         #{table_name}
     SQL
-    @columns.first.map(&:to_sym)
-  end
+      @columns.first.map(&:to_sym)
+    end
 
-  def self.finalize!
-    columns.each do |name|
-      define_method(name) do
-        attributes[name]
-      end
-      define_method("#{name}=") do |val|
-        attributes[name] = val
+    def self.finalize!
+      columns.each do |name|
+        define_method(name) do
+          attributes[name]
+        end
+        define_method("#{name}=") do |val|
+          attributes[name] = val
+        end
       end
     end
-  end
 
-  def self.table_name=(table_name)
-    @table_name = table_name
-  end
+    def self.table_name=(table_name)
+      @table_name = table_name
+    end
 
-  def self.table_name
-    @table_name || self.name.tableize 
-  end
+    def self.table_name
+      @table_name || self.name.tableize 
+    end
 
-  def self.all
-    results = DBConnection.execute(<<-SQL)
+    def self.all
+      results = DBConnection.execute(<<-SQL)
       SELECT
         *
       FROM
         #{table_name}
     SQL
-    parse_all(results)
-  end
+      parse_all(results)
+    end
 
-  def self.parse_all(results)
-     results.map { |attrs| self.new(attrs) }
-  end
+    def self.parse_all(results)
+      results.map { |attrs| self.new(attrs) }
+    end
 
-  def self.find(id)
-    target = DBConnection.execute(<<-SQL, id)
+    def self.find(id)
+      target = DBConnection.execute(<<-SQL, id)
       SELECT
         *
       FROM
@@ -56,38 +57,38 @@ class SQLObject
         ID = ?
     SQL
 
-    return nil if target.empty?
-    self.new(target.first)
-  end
-
-  def initialize(params = {})
-    params.each do |attr_name, val|
-      name_sym = attr_name.to_sym
-      raise Exception.new "unknown attribute '#{attr_name}'" unless self.class.columns.include?(name_sym)
-      send("#{name_sym}=", val)
+      return nil if target.empty?
+      self.new(target.first)
     end
-  end
 
-  def attributes
-    @attributes ||= {}
-  end
+    def initialize(params = {})
+      params.each do |attr_name, val|
+        name_sym = attr_name.to_sym
+        raise Exception.new "unknown attribute '#{attr_name}'" unless self.class.columns.include?(name_sym)
+        send("#{name_sym}=", val)
+      end
+    end
 
-  def attribute_values
-    self.class.columns.map { |attr| send(attr) }
-  end
+    def attributes
+      @attributes ||= {}
+    end
 
-  def insert
-    DBConnection.execute(<<-SQL, *attribute_values)
+    def attribute_values
+      self.class.columns.map { |attr| send(attr) }
+    end
+
+    def insert
+      DBConnection.execute(<<-SQL, *attribute_values)
       INSERT INTO
         #{self.class.table_name}(#{self.class.columns.join(',')})
       VALUES
         (#{(["?"] * attribute_values.length).join(',')})
     SQL
-    self.id = DBConnection.last_insert_row_id
-  end
+      self.id = DBConnection.last_insert_row_id
+    end
 
-  def update
-    DBConnection.execute(<<-SQL, *attribute_values, self.id)
+    def update
+      DBConnection.execute(<<-SQL, *attribute_values, self.id)
       UPDATE
         #{self.class.table_name}
       SET
@@ -95,13 +96,14 @@ class SQLObject
       WHERE
         id = ?
     SQL
-  end
+    end
 
-  def save
-    if self.id.nil?
-      insert
-    else
-      update
+    def save
+      if self.id.nil?
+        insert
+      else
+        update
+      end
     end
   end
 end
